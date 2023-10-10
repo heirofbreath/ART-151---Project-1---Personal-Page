27e7e4083f0ccc424d732d8724fa5e3061d261d5
Milk <mkhan398@uic.edu>
2023-10-10 13:11:23 -0500

    Awesome Hamburger Update (Bookshelf + Guestbook in Library omg)

--- p5js/library.js
+++ p5js/library.js
@@ -37,14 +37,35 @@ function draw(){
     }
 
     // Portrait on Left
+        // push();
+        //     beginShape();
+        //         vertex(5,22);
+        //         vertex(131,96);
+        //         vertex(133,280);
+        //         vertex(8,255);
+        //     endShape(CLOSE);
+        // pop();
+
+    // Detects if mouse is over leftmost front facing bookshelf
+
+    if(mouseX >= 305 && mouseX <= 400){
+        if (mouseY >= 174 && mouseY <= 435){
+            rect(450, 174, 200, 220);
+            text("As of recent, I've been getting back into writing and reading again. My most recent, personal project has to be the SCP article I'm writing, although I'm considering starting it over completely.\n\nWhat about you? If you're reading anything good, or planning on writing something, leave it in the guest book."
+            , 460, 194, 180, 200);
+            let link = createA("https://milknolactose.123guestbook.com/", "Guestbook");
+            link.position(0, 25);
+        }   
+    }
+    
+
+    // Bookshelf
     // push();
     //     beginShape();
-    //         vertex(5,22);
-    //         vertex(131,96);
-    //         vertex(133,280);
-    //         vertex(8,255);
+    //         vertex(305,174)
+    //         vertex(400,174);
+    //         vertex(400,435);
+    //         vertex(305,435);
     //     endShape(CLOSE);
     // pop();
-
-    //
 }
